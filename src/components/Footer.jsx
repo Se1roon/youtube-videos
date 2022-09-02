84c61bf43506264f6bd8d7a95bf8f1d8ccdff808
Se1roon <se1roonm@gmail.com>
2022-09-02 19:30:02 +0200

    Implement Footer component.

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+const Footer = ({ content }) => {
+  return (
+    <footer className="footer">
+      Coded by{" "}
+      <span>
+        <a href="https://github.com/Se1roon">Se1roon</a>
+      </span>
+    </footer>
+  );
+};
+
+export default Footer;
