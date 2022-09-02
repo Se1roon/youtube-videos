5a68e5afb10a26043250fbfb61315550f8a40abc
Se1roon <se1roonm@gmail.com>
2022-09-02 19:04:21 +0200

    Add routing.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import NavBar from "./components/NavBar";
+import { Outlet } from "react-router-dom";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <NavBar />
+      <Outlet />
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import App from "./App";
 import "./assets/css/main.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Navigate to="home" />} />
+        <Route path="/" element={<App />}>
+          <Route path="search" element={<h1>Siema search</h1>} />
+          <Route path="home" element={<h1>Siema home</h1>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
