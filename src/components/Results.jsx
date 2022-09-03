d5fd25f64e154c7f585e893724b310b674e3bfcd
Se1roon <se1roonm@gmail.com>
2022-09-03 20:13:57 +0200

    Implement inputs ability to go disabled when already showing results.
    The reason is because when we searched for some channel we could type in inputs and this would cause errors.

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,7 +1,27 @@
 import { useOutletContext } from "react-router";
+import { useEffect } from "react";
 
 const Results = () => {
   const [channelId, setChannelId, videoId, setVideoId] = useOutletContext();
+
+  useEffect(() => {
+    document.querySelector("#channelId").setAttribute("disabled", true);
+    document.querySelector("#videoId").setAttribute("disabled", true);
+
+    return function () {
+      if (
+        !(
+          document.querySelector("#channelId") &&
+          document.querySelector("#videoId")
+        )
+      )
+        return;
+
+      document.querySelector("#channelId").removeAttribute("disabled");
+      document.querySelector("#videoId").removeAttribute("disabled");
+    };
+  });
+
   return (
     <div>
       <p>{channelId}</p>
